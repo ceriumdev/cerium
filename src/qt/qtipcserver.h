8cd1924fd75bba261ed4be3c841fd390736f4698
Cerium <cerium.coin@gmx.com>
2015-06-07 00:03:32 +0200

    First Commit

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define Cerium-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "CeriumURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
